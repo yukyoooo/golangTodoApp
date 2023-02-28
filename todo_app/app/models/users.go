eaa9c5db48cfef0bf425899154189a0da8700967
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-01 08:17:41 +0900

    データベース作成

---
+++ todo_app/app/models/users.go
@@ -0,0 +1,70 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	ID        int
+	UUID      string
+	Name      string
+	Email     string
+	PassWord  string
+	CreatedAt time.Time
+}
+
+func (u *User) CreateUser() (err error) {
+	cmd := `insert into users (
+		uuid,
+		name,
+		email,
+		password,
+		created_at) values (?, ?, ?, ?, ?)`
+
+	_, err = Db.Exec(
+		cmd,
+		createUUID(),
+		u.Name,
+		u.Email,
+		Encrypt(u.PassWord),
+		time.Now())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetUser(id int) (user User, err error) {
+	user = User{}
+	cmd := `SELECT id, uuid, name, email, password, created_at
+	from users where id = ?`
+	err = Db.QueryRow(cmd, id).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt,
+	)
+	return user, err
+}
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `update users set name = ?, email = ? where id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
